46e3ab32d4f5ab1886004959f5c95746ab4ed140
Arthur Leite <arthur.leite@pricehubble.com>
2026-07-30 11:06:50 +0200

    Change __ to _ & Change default limit and seed articles

--- packages/web-crawler/src/web_crawler/wikipedia/crawler.py
+++ packages/web-crawler/src/web_crawler/wikipedia/crawler.py
@@ -20,14 +20,14 @@ from .utils import SlidingWindowLog, rate_limited
 class WikipediaScraper:
     def __init__(self, seed_articles: list[str] | None = None) -> None:
         self.base_url = WIKIPEDIA_API_PHP_URL
-        self.seed_articles = seed_articles or ["Jesus", "Python (programming language)"]
+        self.seed_articles = seed_articles or ["Jesus"]
 
-        self.__start_robots_parser()
+        self._start_robots_parser()
         self.swl = SlidingWindowLog(self.requests_count, self.requests_duration, minimum_delay=1)
         self.redis = redis.Redis()
         self.redis_set = "wikipedia"
 
-    def __start_robots_parser(self):
+    def _start_robots_parser(self):
         self.robots_parser = RobotFileParser()
         self.robots_parser.set_url(urljoin(self.base_url, "/robots.txt"))
         self.robots_parser.read()
@@ -39,16 +39,16 @@ class WikipediaScraper:
             self.requests_count = REQUESTS_COUNT_DEFAULT
             self.requests_duration = REQUESTS_DURATION_DEFAULT
 
-    async def __get_request(self, session: aiohttp.ClientSession, params: dict[str, str]) -> dict:
+    async def _get_request(self, session: aiohttp.ClientSession, params: dict[str, str]) -> dict:
         async with session.get(self.base_url, params=params, headers=HEADERS) as response:
             if response.status != 200:
                 response.raise_for_status()
             return await response.json()
 
-    def __get_text_params(self, title: str) -> dict[str, str]:
+    def _get_text_params(self, title: str) -> dict[str, str]:
         return {"action": "query", "prop": "extracts", "format": "json", "explaintext": "1", "titles": title}
 
-    def __get_links_params(self, title: str, plcontinue: dict[str, str] | None = None) -> dict[str, str]:
+    def _get_links_params(self, title: str, plcontinue: dict[str, str] | None = None) -> dict[str, str]:
         params = {"action": "query", "prop": "links", "format": "json", "pllimit": "max"} | {"titles": title}
 
         if plcontinue:
@@ -57,11 +57,11 @@ class WikipediaScraper:
             return params
 
     @rate_limited
-    async def __request_text(self, session: aiohttp.ClientSession, title: str) -> None:
+    async def _request_text(self, session: aiohttp.ClientSession, title: str) -> None:
         if self.redis.sismember(self.redis_set, title):
             return
 
-        response = await self.__get_request(session, params=self.__get_text_params(title))
+        response = await self._get_request(session, params=self._get_text_params(title))
 
         if "continue" in response:
             print("There is a continue on the text content response")
@@ -77,9 +77,9 @@ class WikipediaScraper:
             self.redis.sadd(self.redis_set, title)
 
     @rate_limited
-    async def __request_links(self, session: aiohttp.ClientSession, title: str):
+    async def _request_links(self, session: aiohttp.ClientSession, title: str):
         titles = []
-        response = await self.__get_request(session, params=self.__get_links_params(title))
+        response = await self._get_request(session, params=self._get_links_params(title))
 
         pages = response["query"]["pages"]
 
@@ -87,8 +87,8 @@ class WikipediaScraper:
             titles.extend([link["title"] for link in page["links"]])
 
         while "continue" in response:
-            response = await self.__get_request(
-                session, params=self.__get_links_params(title, plcontinue=response["continue"])
+            response = await self._get_request(
+                session, params=self._get_links_params(title, plcontinue=response["continue"])
             )
 
             pages = response["query"]["pages"]
@@ -98,9 +98,7 @@ class WikipediaScraper:
 
         return titles
 
-    async def __crawl_worker(
-        self, session: aiohttp.ClientSession, queue: asyncio.Queue, visited: set, pages_limit: int = 50
-    ):
+    async def _crawl_worker(self, session: aiohttp.ClientSession, queue: asyncio.Queue, visited: set, pages_limit: int):
         pages_crawled = 0
         while pages_crawled < pages_limit:
             title = await queue.get()
@@ -109,15 +107,15 @@ class WikipediaScraper:
                 print(f"Page {title} has already been visited")
                 continue
 
-            await self.__request_text(session, title)
-            new_titles = await self.__request_links(session, title)
+            await self._request_text(session, title)
+            new_titles = await self._request_links(session, title)
 
             for title in new_titles:
                 queue.put_nowait(title)
 
             pages_crawled += 1
 
-    async def crawl(self, pages_limit: int = 5):
+    async def crawl(self, pages_limit: int = 50):
         queue = asyncio.Queue()
         for article in self.seed_articles:
             queue.put_nowait(article)
@@ -126,7 +124,7 @@ class WikipediaScraper:
 
         async with aiohttp.ClientSession() as session:
             await asyncio.gather(
-                *[self.__crawl_worker(session, queue, visited, pages_limit) for _ in range(CONCURRENCY)]
+                *[self._crawl_worker(session, queue, visited, pages_limit) for _ in range(CONCURRENCY)]
             )
 
 
