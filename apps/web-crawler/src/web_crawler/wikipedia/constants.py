d063022423b9c9cf49874f5a2c94d88c283aae7f
Arthur Leite <arthur.leite@pricehubble.com>
2026-08-24 18:39:46 +0200

    Removing dir creation leftover of older commit

--- apps/web-crawler/src/web_crawler/wikipedia/constants.py
+++ apps/web-crawler/src/web_crawler/wikipedia/constants.py
@@ -1,5 +1,3 @@
-import os
-
 HEADERS = {"User-Agent": "WikiProjCrawlBot/0.1 (1szhae97d@mozmail.com)", "Accept-Encoding": "gzip"}
 
 WIKIPEDIA_API_HTML_URL = "https://en.wikipedia.org/api/rest_v1/page/html/"
@@ -7,6 +5,3 @@ WIKIPEDIA_API_PHP_URL = "https://en.wikipedia.org/w/api.php"
 REQUESTS_COUNT_DEFAULT = 20
 REQUESTS_DURATION_DEFAULT = 60
 CONCURRENCY = 2
-
-OUTPUT_DIR = os.getenv("OUTPUT_DIR", "/usr/src/app/crawled_data/")
-os.makedirs(OUTPUT_DIR, exist_ok=True)
