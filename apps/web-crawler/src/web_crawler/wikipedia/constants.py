20bfe31e62edcf306b422144cd033db6b3d7d437
Arthur Leite <arthur.leite@pricehubble.com>
2026-08-25 17:35:55 +0200

    Increase number of requests per minute

--- apps/web-crawler/src/web_crawler/wikipedia/constants.py
+++ apps/web-crawler/src/web_crawler/wikipedia/constants.py
@@ -2,6 +2,6 @@ HEADERS = {"User-Agent": "WikiProjCrawlBot/0.1 (1szhae97d@mozmail.com)", "Accept
 
 WIKIPEDIA_API_HTML_URL = "https://en.wikipedia.org/api/rest_v1/page/html/"
 WIKIPEDIA_API_PHP_URL = "https://en.wikipedia.org/w/api.php"
-REQUESTS_COUNT_DEFAULT = 20
+REQUESTS_COUNT_DEFAULT = 45
 REQUESTS_DURATION_DEFAULT = 60
 CONCURRENCY = 2
